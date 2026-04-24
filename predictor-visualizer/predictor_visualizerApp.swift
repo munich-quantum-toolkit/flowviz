ae82d69385fb7b8c616b989f43f7f31435ecbd24
Linus <42340116+linus-hologram@users.noreply.github.com>
2026-04-24 10:39:05 +0200

    Initial Commit

---
+++ predictor-visualizer/predictor_visualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  predictor_visualizerApp.swift
+//  predictor-visualizer
+//
+//  Created by Linus Bohle on 24.04.26.
+//
+
+import SwiftUI
+
+@main
+struct predictor_visualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
