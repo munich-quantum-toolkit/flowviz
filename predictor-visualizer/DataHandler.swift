1686793284c2ccccbbd334fc12e63a8a4c7e5115
Linus <42340116+linus-hologram@users.noreply.github.com>
2026-04-24 12:19:14 +0200

    added first (prototyping) draft of DataHandler class responsible for data handling.

---
+++ predictor-visualizer/DataHandler.swift
@@ -0,0 +1,51 @@
+//
+//  DataHandler.swift
+//  predictor-visualizer
+//
+//  Created by Linus Bohle on 24.04.26.
+//
+
+
+import Foundation
+import SwiftUI
+import OSLog
+
+@Observable class DataHandler {
+    /// Enumeration containing all possible errors occuring together with the ``DataHandler`` class.
+    ///
+    /// All enumerations are accompanied by an associated string value, which should be a user-friendly error message
+    /// describing what went wrong.
+    enum DataError: Error {
+        case AccessFailed(String)
+        case DecodingError(String)
+    }
+
+    var traces: [CompilationTracer] = []
+    let logger: Logger = Logger(subsystem: "DataHandling", category: "DataHandler")
+
+    /// Parses a JSON file from a given URL and adds it to the traces array.
+    /// - Parameter url: the URL of the file that should be parsed.
+    func importTrace(from url: URL) throws {
+        // Security wrapper required for accessing files outside the app's immediate sandbox (like Finder/Files app)
+        guard url.startAccessingSecurityScopedResource() else {
+            logger.error("Failed to access security scoped resource: \(url.path)")
+            throw DataError.AccessFailed("Failed to access security scoped resource.")
+        }
+        
+        defer { url.stopAccessingSecurityScopedResource() }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let newTrace = try decoder.decode(CompilationTracer.self, from: data)
+            self.traces.append(newTrace)
+            logger.info("Successfully imported: \(newTrace.circuitName) from \(url.path)")
+        } catch {
+            logger.error("Failed to decode trace JSON: \(error)")
+            throw DataError.DecodingError("Failed to decode trace JSON: \(error)")
+        }
+    }
+}
